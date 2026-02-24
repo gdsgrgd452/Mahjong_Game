be6f83f9893cb72e4265933891b4cb4e053c164d
gdsgrgd452 <bobharrison2005@gmail.com>
2026-02-24 16:33:31 +0000

    Initial setup commit

---
+++ src/main/java/com/example/mahjong_game/MahjongGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.mahjong_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MahjongGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mahjong_game/MahjongGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mahjong_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
