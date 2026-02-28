6a291ab33484b977daf12517d31595f9e19eec94
gdsgrgd452 <bobharrison2005@gmail.com>
2026-02-28 12:14:51 +0000

    Added helper functions for a variety of things

---
+++ src/main/java/com/example/mahjong_game/logic/util/HelperFunctions.java
@@ -0,0 +1,84 @@
+package com.example.mahjong_game.logic.util;
+
+import com.example.mahjong_game.model.Game;
+import com.example.mahjong_game.model.Player;
+import com.example.mahjong_game.model.tiles.Tile;
+import com.example.mahjong_game.service.PlayerService;
+import com.example.mahjong_game.service.TileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+@Service
+public class HelperFunctions {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelperFunctions.class);
+
+    private final Random r = new Random();
+
+    private final TileService tileService;
+    private final PlayerService playerService;
+    private final ComparisonHelperFunctions c;
+
+    @Autowired
+    public HelperFunctions(TileService tileService, PlayerService playerService, ComparisonHelperFunctions c) {
+        this.tileService = tileService;
+        this.playerService = playerService;
+        this.c = c;
+    }
+
+    public Tile getRandomTile(Game game) {
+        List<Tile> allTiles = tileService.getLiveTiles(game); //Only unused ones
+        return allTiles.get(r.nextInt(allTiles.size()));
+    }
+
+    public Tile addTileToPlayer(Player player, Tile tile) {
+        logger.info("Adding specific tile to player: {}", player.getUsername());
+        tile.setJustPickedUp(true);
+        return playerService.addTileToHand(player.getPlayerId(), tile);
+    }
+
+    public Tile addRandomTileToPlayer(Game game, Player player) {
+        logger.info("Adding random tile to player: {}", player.getUsername());
+        return playerService.addTileToHand(player.getPlayerId(), getRandomTile(game));
+    }
+
+    public List<Tile> simulateHandWithNewTileForChecks(Player player, Tile tile) {
+        List<Tile> playerTiles = new ArrayList<>(player.getCurrentHandNoPlaced());
+        playerTiles.add(tile);
+        playerTiles.sort(c.getTileLogicComparator());
+        return playerTiles;
+    }
+
+    public Player iterateThroughListWithLooping(List<Player> players, Player playerTurn) {
+        if (players == null || players.isEmpty()) {
+            return null;
+        }
+
+        int currentIndex = players.indexOf(playerTurn);
+        if (currentIndex == -1) currentIndex = 0;
+        int nextIndex = (currentIndex + 1) % players.size(); //Ensures looping
+        return players.get(nextIndex);
+    }
+
+    public List<Tile> filterListOfTilesForNoPungOrChow(List<Tile> currentHand) {
+        return currentHand.stream().filter(t -> t.getChow() == null && t.getPung() == null).toList();
+    }
+
+    public void decreaseAmountRemainingForAllSameTiles(Game game, Integer amountRevealed, Tile tile) {
+        String tileInfo = tile.getSuit() + "-" + tile.getNumber();
+        Map<String, List<Tile>> groupedBySAndN = c.groupTilesBySuitAndNumber(tileService.findAllTilesByGame(game)); // Groups all tiles in the format e.g. <Bamboo-3: [t1, t2, t3]>
+        for (Tile t : groupedBySAndN.get(tileInfo)) {
+            t.setAmountRemaining(t.getAmountRemaining() - amountRevealed);
+            tileService.saveTile(t);
+        }
+        logger.info("Decreased amount remaining for tiles: {} to: {}", tileInfo, tile.getAmountRemaining());
+    }
+
+}
