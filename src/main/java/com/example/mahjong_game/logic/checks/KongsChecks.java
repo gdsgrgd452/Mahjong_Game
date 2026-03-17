5328c46c3219f571192554bd4be9da33099cffb7
gdsgrgd452 <bobharrison2005@gmail.com>
2026-03-17 01:42:41 +0000

    Added checks for kongs

--- src/main/java/com/example/mahjong_game/logic/checks/KongsChecks.java
+++ src/main/java/com/example/mahjong_game/logic/checks/KongsChecks.java
@@ -1,7 +1,6 @@
 package com.example.mahjong_game.logic.checks;
 
 import com.example.mahjong_game.logic.util.ComparisonHelperFunctions;
-import com.example.mahjong_game.logic.util.HelperFunctions;
 import com.example.mahjong_game.model.Player;
 import com.example.mahjong_game.model.actions.Action;
 import com.example.mahjong_game.model.tiles.SuitedTile;
@@ -15,13 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class KongsChecks {
 
-    private final HelperFunctions h;
     private final ComparisonHelperFunctions c;
     private final ActionService actionService;
     private final TileService tileService;
@@ -29,8 +28,7 @@ public class KongsChecks {
     private static final Logger logger = LoggerFactory.getLogger(KongsChecks.class);
 
     @Autowired
-    public KongsChecks(HelperFunctions h, ComparisonHelperFunctions c, ActionService actionService, TileService tileService, PlayerService playerService) {
-        this.h = h;
+    public KongsChecks(ComparisonHelperFunctions c, ActionService actionService, TileService tileService, PlayerService playerService) {
         this.c = c;
         this.actionService = actionService;
         this.tileService = tileService;
@@ -38,6 +36,7 @@ public class KongsChecks {
     }
 
     public Player lookForKongs(Player player, List<Tile> currentHand) {
+        boolean validKongSaved = false;
         List<Action> pungs = actionService.findPlayersActionsByType(player, "Pung");
         logger.info("Found {} pungs when trying to find kongs (Start/Pickup)", pungs.size());
 
@@ -46,24 +45,43 @@ public class KongsChecks {
 
             if (pungFirstTile instanceof SuitedTile) {
                 String pungInfo = pungFirstTile.getSuit() + "-" + pungFirstTile.getNumber(); //Gets e.g. Bamboo-7
-                Map<String, List<Tile>> tilesGroupedBySuit = c.groupTilesBySuitAndNumber(currentHand); // Groups all tiles in the format e.g. <Bamboo-7: [t1, t2, t3]>
-
-                if (tilesGroupedBySuit.containsKey(pungInfo)) {
-                    List<Integer> tileIds = new ArrayList<>(pung.getTiles().stream().map(Tile::getTileId).toList());
-                    tileIds.add(tilesGroupedBySuit.get(pungInfo).getFirst().getTileId());
-                    handleKong(player, tileIds);
+                Map<String, List<Tile>> tilesGrouped = c.groupTilesBySuitAndNumber(currentHand); // Groups all tiles in the format e.g. <Bamboo-7: [t1, t2, t3]
+                List<Integer> kongIds = findTileToAddToKong(tilesGrouped, pung, pungInfo);
+                if (tryHandleKong(player, kongIds)) {
+                    tilesGrouped.remove(pungInfo); //Prevents overlaps
+                    validKongSaved = true;
                 }
-
-
             } else {
                 String pungInfo = pungFirstTile.getSuit();
-                Map<String, List<Tile>> tilesGroupedBySuit = c.groupTilesBySuit(currentHand);
-
-                if (tilesGroupedBySuit.containsKey(pungInfo)) {
-
+                Map<String, List<Tile>> tilesGrouped = c.groupTilesBySuit(currentHand);
+                List<Integer> kongIds = findTileToAddToKong(tilesGrouped, pung, pungInfo);
+                if (tryHandleKong(player, kongIds)) {
+                    tilesGrouped.remove(pungInfo); //Prevents overlaps
+                    validKongSaved = true;
                 }
             }
         }
+        return validKongSaved ? player : null;
+    }
+
+    public List<Integer> findTileToAddToKong(Map<String, List<Tile>> tilesGroupedBySuit, Action pung, String pungInfo) {
+        if (tilesGroupedBySuit.containsKey(pungInfo)) { //If there is another tile with the same suit and number
+            List<Integer> tileIds = new ArrayList<>(pung.getTiles().stream().map(Tile::getTileId).toList());
+            tileIds.add(tilesGroupedBySuit.get(pungInfo).getFirst().getTileId());
+            return tileIds;
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * Actually saves the kong
+     */
+    public boolean tryHandleKong(Player player, List<Integer> kongIds) {
+        if (!kongIds.isEmpty() && handleKong(player, kongIds)) { //Actually saves
+                logger.info("Kong found for player: {}", player.getUsername());
+                return true;
+            }
+        return false;
     }
 
     public boolean handleKong(Player player, List<Integer> kongTilesIds ) {
